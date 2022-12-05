e4881e88654a928bf8b1d8d36c96ecb0246c118e
Aleksudv <107849168+Aleksudv@users.noreply.github.com>
2022-12-05 11:37:27 +0000

    Deploying to gh-pages from @ Aleksudv/goit-js-hw-09@77454d7253d163be8c1aa80d79887e6523241502 🚀

---
+++ 01-color-switcher.239d892b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");let d=document.querySelector("body"),l=null;t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,l=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(l)})),console.log(d);
+//# sourceMappingURL=01-color-switcher.239d892b.js.map
